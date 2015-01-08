996ac527df67fd443be987ddaa52735da99f530b
Shrikant <shrikant.havale@yahoo.com>
2015-01-08 14:43:12 +0100

    Test Suite - All Test Cases

---
+++ src/at/fhooe/mc/test/AllTests.java
@@ -0,0 +1,19 @@
+package at.fhooe.mc.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+/**
+ * This class executes all the test cases
+ * 
+ * @author Metrics_Testing Team
+ * Jan 8, 2015
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ ElevatorAdapterTest.class, ElevatorManualModeTest.class })
+public class AllTests {
+
+}
